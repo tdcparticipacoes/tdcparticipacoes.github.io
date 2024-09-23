fdc566318748592df871efe584135b0cc9955a92
tdcparticipacoes <tdcparticipacoes@gmail.com>
2024-09-23 18:12:26 -0300

    Adição de funcionalidade de envio de e-mail

--- js/custom.js
+++ js/custom.js
@@ -1,46 +1,81 @@
 // to get current year
 function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
+  var currentDate = new Date();
+  var currentYear = currentDate.getFullYear();
+  document.querySelector("#displayYear").innerHTML = currentYear;
 }
 
 getYear();
 
-
 // client section owl carousel
 $(".client_owl-carousel").owlCarousel({
-    loop: true,
-    margin: 20,
-    dots: false,
-    nav: true,
-    navText: [],
-    autoplay: true,
-    autoplayHoverPause: true,
-    navText: [
-        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-    ],
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 2
-        },
-        1000: {
-            items: 2
-        }
-    }
+  loop: true,
+  margin: 20,
+  dots: false,
+  nav: true,
+  navText: [],
+  autoplay: true,
+  autoplayHoverPause: true,
+  navText: [
+    '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+    '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+  ],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 2,
+    },
+  },
 });
 
-
-
 /** google_map js **/
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+// function myMap() {
+//     var mapProp = {
+//         center: new google.maps.LatLng(40.712775, -74.005973),
+//         zoom: 18,
+//     };
+//     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+// }
+
+var form = document.getElementById("form-newsletter");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
+//   var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        // status.innerHTML = "Thanks for your submission!";
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            // status.innerHTML = data["errors"]
+            //   .map((error) => error["message"])
+            //   .join(", ");
+          } else {
+            // status.innerHTML = "Oops! There was a problem submitting your form";
+          }
+        });
+      }
+    })
+    .catch((error) => {
+    //   status.innerHTML = "Oops! There was a problem submitting your form";
+    });
 }
+
+form.addEventListener("submit", handleSubmit);
